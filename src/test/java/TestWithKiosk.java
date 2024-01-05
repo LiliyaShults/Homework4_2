e3e350a4b77967dd3ef672ae96470bdcb4aa92e6
Your Name <you@example.com>
2024-01-06 00:30:00 +0300

    first commit

---
+++ src/test/java/TestWithKiosk.java
@@ -0,0 +1,55 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TestWithKiosk {
+    WebDriver driver;
+
+    @BeforeAll
+    public static void driverSetup() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void driverStart() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--kiosk");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    public void driverStop() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void headless() throws InterruptedException {
+
+        driver.get("https://demo.w3layouts.com/demos_new/template_demo/03-10-2020/photoflash-liberty-demo_Free/685659620/web/index.html?_" +
+                "ga=2.181802926.889871791.1632394818-2083132868.1632394818");
+
+        driver.findElement(By.xpath("/html/body/section[2]/div/ul[2]/li[1]/span/a/div[1]"))
+                .click();
+        Thread.sleep(3000);
+
+        String present;
+        try {
+            driver.findElement(By.className("pp_content_container"));
+            present ="Модальное окно было открыто. Картинка показалась после клика.";
+        } catch (NoSuchElementException e) {
+            present = "Модальное окно не было открыто. Картинка не показалась после клика.";
+        }
+        System.out.println(present);
+
+    }
+
+    }
